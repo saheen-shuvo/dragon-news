c528cc7ae32b661e3dc88e4228b85ad9905da828
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2024-11-17 01:25:53 +0600

    Left Navbar designed

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layoutComponent/LeftNavbar";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
@@ -17,7 +18,9 @@ const HomeLayout = () => {
         </section>
       </nav>
       <main className="w-11/12 mx-auto pt-5 grid grid-cols-12">
-      <aside className="left col-span-3">Left Nav bar</aside>
+      <aside className="left col-span-3">
+        <LeftNavbar></LeftNavbar>
+      </aside>
       <section className="col-span-6">Main Content</section>
       <aside className="col-span-3">Right nav bar</aside>
       </main>
